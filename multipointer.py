460158eaf402c559659c41580a9cb724fa7cd646
gamingfail123 <lekahade@gmail.com>
2017-02-18 18:13:00 +0100

    Added the file

---
+++ multipointer.py
@@ -0,0 +1,70 @@
+import builtins
+
+class MultiPointer:
+    def index(self, *index):
+        if len(index) > 1:
+            next = self[index[0]]
+            if type(next).__name__ in globals() and "index" in globals()[type(next).__name__]().__dir__():
+                return globals()[type(next).__name__](self[index[0]]).index(*index[1:])
+        else:
+            return self[index[0]]
+
+
+class dict(builtins.dict):
+    index = MultiPointer.index
+
+
+class list(builtins.list):
+    index = MultiPointer.index
+
+
+class str(builtins.str):
+    index = MultiPointer.index
+
+
+class tuple(builtins.tuple):
+    index = MultiPointer.index
+
+
+class bytearray(builtins.bytearray):
+    index = MultiPointer.index
+
+
+class bytes(builtins.bytes):
+    index = MultiPointer.index
+
+
+class enumerate(builtins.enumerate):
+    index = MultiPointer.index
+
+
+class frozenset(builtins.frozenset):
+    index = MultiPointer.index
+
+
+class map(builtins.map):
+    index = MultiPointer.index
+
+
+class set(builtins.set):
+    index = MultiPointer.index
+
+
+class zip(builtins.zip):
+    index = MultiPointer.index
+
+def index(obj, *keys):
+    return MultiPointer.index(obj, *keys)
+
+
+
+if __name__ == "__main__":
+
+    d = {
+        "hi": {
+            "okay": {
+                "no": (0, 1, 2, 3)
+            }
+        }
+    }
+    print(index(d, "hi", "okay", "no", 3))
